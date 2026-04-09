ade3021cdbf8b75d2ee4309bc4776ef04d736a9c
JTD <teixeira.dias@gmail.com>
2026-04-09 16:24:17 +0100

    phone-heart-v§

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -70,7 +70,7 @@ export default function Header() {
               {/* Mobile: Heart-shaped call button */}
               <a
                 href="tel:+351210136276"
-                className="sm:hidden block w-10 h-10 relative hover:opacity-80 transition-opacity"
+                className="sm:hidden block w-[50px] h-[50px] relative hover:opacity-80 transition-opacity translate-x-[10px]"
                 style={{
                   animation: 'heartbeat 1.5s ease-in-out infinite'
                 }}
@@ -83,7 +83,7 @@ export default function Header() {
                 >
                   <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                 </svg>
-                <Phone className="absolute left-1/2 -translate-x-1/2 top-[10px] w-4 h-4 text-white pointer-events-none" />
+                <Phone className="absolute left-1/2 -translate-x-1/2 top-[13px] w-[18px] h-[18px] text-white pointer-events-none" />
               </a>
             </div>
 
