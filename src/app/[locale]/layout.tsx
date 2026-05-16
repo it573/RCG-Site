b5ecf43f5cc77d4da5eff4f407bf0ef462f2449e
JTD <teixeira.dias@gmail.com>
2026-05-16 11:06:32 +0100

    pixel-removed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -75,32 +75,6 @@ export default async function LocaleLayout({
     <html lang={locale}>
       <head>
         <meta name="facebook-domain-verification" content="flm8qq5atlf8dsoijnxd7yqt6aiwh7" />
-        <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '27646133138319774');
-              fbq('track', 'PageView');
-            `
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{display:'none'}}
-            src="https://www.facebook.com/tr?id=27646133138319774&ev=PageView&noscript=1"
-          />
-        </noscript>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
